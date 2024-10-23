380b89b89051218e88d9415a3a1c2f0441193465
Zarutska Alina <zaryckaalina@gmail.com>
2024-10-23 11:07:49 +0300

    update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,12 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const openModalBtn = document.querySelector("[data-modal-open]");
+  const closeModalBtn = document.querySelector("[data-modal-close]");
+  const modal = document.querySelector("[data-modal]");
+
+  openModalBtn.addEventListener("click", () =>
+    modal.classList.toggle("is-hidden")
+  );
+  closeModalBtn.addEventListener("click", () =>
+    modal.classList.toggle("is-hidden")
+  );
+})();
